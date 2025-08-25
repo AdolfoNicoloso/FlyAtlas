7e4a95ff5b1cbe24b1b9e02d261b2dfa422f6033
AdolfoNicoloso <161170811+AdolfoNicoloso@users.noreply.github.com>
2025-08-25 10:37:39 -0700

    fix: use CommonJS in PostCSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 // Tailwind for NativeWind (RN)
 module.exports = {
-  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   presets: [require('nativewind/preset')],
   theme: {
     extend: {
